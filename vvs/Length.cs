f9a9aa738c438181dc4aa31391000cc3adc0e457
Анна Шелест <anna.shelest.02@bk.ru>
2021-11-06 18:35:45 +0300

    Добавьте файлы проекта.

---
+++ vvs/Length.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vvs
+{
+    public class Length
+    {
+        private double value;
+        private string type;
+
+        public Length(double value, string type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+       
+        public string Verbose()
+        {
+            return String.Format("{0} {1}", this.value, this.type);
+        }
+    }
+}
