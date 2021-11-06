c112e5dbb7a63748a32dc68dad101b7fed9d88ef
Анна Шелест <anna.shelest.02@bk.ru>
2021-11-07 00:30:38 +0300

    обновление дизайна

--- vvs/Form1.Designer.cs
+++ vvs/Form1.Designer.cs
@@ -36,6 +36,9 @@ namespace vvs
             this.cmbFirstType = new System.Windows.Forms.ComboBox();
             this.cmbSecondType = new System.Windows.Forms.ComboBox();
             this.cmbResultType = new System.Windows.Forms.ComboBox();
+            this.txtKf = new System.Windows.Forms.TextBox();
+            this.txtKfFirst = new System.Windows.Forms.TextBox();
+            this.txtKfSecond = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // cmbOperation
@@ -44,16 +47,16 @@ namespace vvs
             this.cmbOperation.Items.AddRange(new object[] {
             "+",
             "-"});
-            this.cmbOperation.Location = new System.Drawing.Point(41, 34);
+            this.cmbOperation.Location = new System.Drawing.Point(39, 30);
             this.cmbOperation.Name = "cmbOperation";
-            this.cmbOperation.Size = new System.Drawing.Size(55, 24);
+            this.cmbOperation.Size = new System.Drawing.Size(69, 24);
             this.cmbOperation.TabIndex = 0;
             this.cmbOperation.Text = "+";
             this.cmbOperation.SelectedIndexChanged += new System.EventHandler(this.cmbOperation_SelectedIndexChanged);
             // 
             // txtFirst
             // 
-            this.txtFirst.Location = new System.Drawing.Point(129, 21);
+            this.txtFirst.Location = new System.Drawing.Point(141, 30);
             this.txtFirst.Name = "txtFirst";
             this.txtFirst.Size = new System.Drawing.Size(121, 22);
             this.txtFirst.TabIndex = 1;
@@ -61,7 +64,7 @@ namespace vvs
             // 
             // txtSecond
             // 
-            this.txtSecond.Location = new System.Drawing.Point(129, 58);
+            this.txtSecond.Location = new System.Drawing.Point(141, 67);
             this.txtSecond.Name = "txtSecond";
             this.txtSecond.Size = new System.Drawing.Size(121, 22);
             this.txtSecond.TabIndex = 2;
@@ -69,15 +72,15 @@ namespace vvs
             // 
             // txtResult
             // 
-            this.txtResult.Location = new System.Drawing.Point(41, 95);
+            this.txtResult.Location = new System.Drawing.Point(39, 104);
             this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(209, 22);
+            this.txtResult.Size = new System.Drawing.Size(223, 22);
             this.txtResult.TabIndex = 3;
             // 
             // cmbFirstType
             // 
             this.cmbFirstType.FormattingEnabled = true;
-            this.cmbFirstType.Location = new System.Drawing.Point(273, 21);
+            this.cmbFirstType.Location = new System.Drawing.Point(284, 30);
             this.cmbFirstType.Name = "cmbFirstType";
             this.cmbFirstType.Size = new System.Drawing.Size(164, 24);
             this.cmbFirstType.TabIndex = 4;
@@ -86,7 +89,7 @@ namespace vvs
             // cmbSecondType
             // 
             this.cmbSecondType.FormattingEnabled = true;
-            this.cmbSecondType.Location = new System.Drawing.Point(273, 56);
+            this.cmbSecondType.Location = new System.Drawing.Point(284, 65);
             this.cmbSecondType.Name = "cmbSecondType";
             this.cmbSecondType.Size = new System.Drawing.Size(164, 24);
             this.cmbSecondType.TabIndex = 5;
@@ -95,17 +98,42 @@ namespace vvs
             // cmbResultType
             // 
             this.cmbResultType.FormattingEnabled = true;
-            this.cmbResultType.Location = new System.Drawing.Point(273, 95);
+            this.cmbResultType.Location = new System.Drawing.Point(284, 104);
             this.cmbResultType.Name = "cmbResultType";
             this.cmbResultType.Size = new System.Drawing.Size(164, 24);
             this.cmbResultType.TabIndex = 6;
             this.cmbResultType.SelectedIndexChanged += new System.EventHandler(this.cmbResultType_SelectedIndexChanged);
             // 
+            // txtKf
+            // 
+            this.txtKf.Location = new System.Drawing.Point(39, 67);
+            this.txtKf.Name = "txtKf";
+            this.txtKf.Size = new System.Drawing.Size(69, 22);
+            this.txtKf.TabIndex = 7;
+            // 
+            // txtKfFirst
+            // 
+            this.txtKfFirst.Location = new System.Drawing.Point(463, 30);
+            this.txtKfFirst.Name = "txtKfFirst";
+            this.txtKfFirst.Size = new System.Drawing.Size(114, 22);
+            this.txtKfFirst.TabIndex = 8;
+            // 
+            // txtKfSecond
+            // 
+            this.txtKfSecond.Location = new System.Drawing.Point(463, 65);
+            this.txtKfSecond.Name = "txtKfSecond";
+            this.txtKfSecond.Size = new System.Drawing.Size(114, 22);
+            this.txtKfSecond.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(589, 175);
+            this.Controls.Add(this.txtKfSecond);
+            this.Controls.Add(this.txtKfFirst);
+            this.Controls.Add(this.txtKf);
             this.Controls.Add(this.cmbResultType);
             this.Controls.Add(this.cmbSecondType);
             this.Controls.Add(this.cmbFirstType);
@@ -113,6 +141,7 @@ namespace vvs
             this.Controls.Add(this.txtSecond);
             this.Controls.Add(this.txtFirst);
             this.Controls.Add(this.cmbOperation);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -129,6 +158,9 @@ namespace vvs
         private System.Windows.Forms.ComboBox cmbFirstType;
         private System.Windows.Forms.ComboBox cmbSecondType;
         private System.Windows.Forms.ComboBox cmbResultType;
+        private System.Windows.Forms.TextBox txtKf;
+        private System.Windows.Forms.TextBox txtKfFirst;
+        private System.Windows.Forms.TextBox txtKfSecond;
     }
 }
 
